ae385ed5b41a4f19c560fb24f099bd1524e1cabb
luismede <luis.medeiros@germinare.org.br>
2025-09-17 17:23:11 -0300

    feat(app.module.ts): Adicionar importação do RedisModule

--- nestjs/task-manager-api/task-manager-api/src/app.module.ts
+++ nestjs/task-manager-api/task-manager-api/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from "@nestjs/common";
 import { TaskModule } from './task/task.module'
 import { DatabaseModule } from "./shared/database/database.module";
 import { RouterModule } from "@nestjs/core";
+import { RedisModule } from "redis/redis.module";
 
 @Module({
   imports: [
     DatabaseModule,
     TaskModule,
+    RedisModule,
     RouterModule.register([
       {
         path: 'task',
